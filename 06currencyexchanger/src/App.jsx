f0b7d8eb79c2ddc42dad0be14513fda2e68e2885
Vishad-byte <srivastavavishad0212@gmail.com>
2025-07-16 20:23:29 +0530

    Setup for currency exchanger

---
+++ 06currencyexchanger/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1 className='bg-blue-400 text-3xl text-gray-800 flex align-middle justify-center px-4 py-3 mx-150 my-10 rounded-full '>Currency exchanger</h1>
+    </>
+  )
+}
+
+export default App
