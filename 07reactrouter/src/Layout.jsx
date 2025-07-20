cfaf4acdb80c178ae22db5736e5447d4b30f5752
Vishad-byte <srivastavavishad0212@gmail.com>
2025-07-20 23:51:11 +0530

    Added header and footer component

---
+++ 07reactrouter/src/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Header from './components/Header/Header'
+import { Outlet } from 'react-router-dom'
+
+function Layout() {
+  return (
+    <>
+    <Header/>                   {/*by using this we will be able to create a page with same fixed header and footer with changing content in the middle which will be done by outlet */}
+    <Outlet/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Layout
