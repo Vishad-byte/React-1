de3dc0380749aeb26720f647c119c938666f54ec
Vishad-byte <srivastavavishad0212@gmail.com>
2025-07-22 22:53:25 +0530

    github info added using api

--- 07reactrouter/src/components/Github/Github.jsx
+++ 07reactrouter/src/components/Github/Github.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLoaderData } from 'react-router-dom'
+
 
 function Github() {
+    const data = useLoaderData()
+    // const [ data, setData ] = useState([])
+    // useEffect(() => {
+    //     fetch('https://api.github.com/users/Vishad-byte').
+    //     then(res => res.json()).
+    //     then(data => {
+    //         setData(data)
+    //     })
+    // })
   return (
-    <div className='text-center m-4 bg-gray-700 text-white p-4 text-3xl'>Github followers: </div>
+    <div className='text-center m-4 bg-gray-700 text-white p-4 text-3xl'>Github following: {data.following}</div>
   )
 }
 
 export default Github
+
+export const githubInfoLoader = async () => {
+    const response = await fetch('https://api.github.com/users/Vishad-byte')
+    return response.json()
+}
